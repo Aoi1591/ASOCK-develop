98ea28984a419538a95a8b4c4ae6e81546ad0c9e
Aoi1591 <2401906@s.asojuku.ac.jp>
2025-10-10 10:27:48 +0900

    URL追加

--- src/php/UserHome.php
+++ src/php/UserHome.php
@@ -53,7 +53,7 @@ for ($i = 0; $i < 7 ;$i++) {
 </div>
 <script>
     //後でここにGASのWebアプリURLを入れる
-    const GAS_URL = "";
+    const GAS_URL = "https://discordapp.com/channels/@me/1151361053159071755/1426017822592729111";
 
     //スプレッドシートからデータを取得
     async function loadData(){
